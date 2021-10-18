95c5b0dcce1ba3ab77e032096bfb16b40d71a48a
Emilio Franco <ejfranco06@gmail.com>
2021-10-18 10:54:46 -0400

    Add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "dm-serif-display": ['DM Serif Display', 'serif'],
+        "public-sans": ['Public Sans', 'sans-serif']
+      },
+      colors:{
+        primary:{
+          pink: "#ba4270",
+          white: "#fbfcfe"
+        },
+        secondary:{
+          "san-juan-blue": "#36536b",
+          "mirage-blue": "#1b262f",
+          "charm-pink": "#da6d97",
+          "light-san-juan-blue": "#6c8294"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
